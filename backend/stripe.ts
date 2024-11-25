3cdfcd20c4baf13f7b0bba96a8d1ecd91645f2fc
synderis <toccidylan@gmail.com>
2024-11-25 07:39:51 -0500

    stripe_checkout

--- backend/stripe.ts
+++
@@ -1,8 +0,0 @@
-// import axios from 'axios';
-
-// export const createCheckoutSession = async (productId: number) => {
-//     const response = await axios.post('http://localhost:8000/create-checkout-session/', {
-//         product_id: productId
-//     });
-//     return response.data.checkout_url;
-// };
